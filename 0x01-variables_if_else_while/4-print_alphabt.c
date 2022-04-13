8038b33fa0149a83bfa0527d8902522d000413de
EyobAshenaki <ethioeyoba@gmail.com>
2022-04-13 06:53:20 +0300

    add 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 when excution is done
+ */
+
+int main(void)
+{
+char c = 'a';
+
+while (c <= 'z')
+{
+if (!(c == 'q' || c == 'e'))
+putchar(c);
+c++;
+}
+
+putchar('\n');
+
+return (0);
+}
