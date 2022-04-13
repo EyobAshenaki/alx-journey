af77c9c7f485f4f466c8f2b19f4273d0d3ff8189
EyobAshenaki <ethioeyoba@gmail.com>
2022-04-13 07:00:45 +0300

    add 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 when excution is done
+ */
+
+int main(void)
+{
+int n = 0;
+
+while (n < 10)
+{
+printf("%d", n);
+n++;
+}
+printf("\n");
+
+return (0);
+}
