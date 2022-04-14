d36f8019620e8a7291a347f8098683e34a7a2cdd
EyobAshenaki <ethioeyoba@gmail.com>
2022-04-14 22:21:26 +0300

    fix 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,16 +9,16 @@
 int main(void)
 {
 
-    int i, count = 1024, sum;
+int i, count = 1024, sum;
 
-    for (i = 0; i < count; i++)
-    {
-        if (i % 3 == 0 || i % 5 == 0)
-        {
-            sum += i;
-        }
-    }
-    printf("%d\n", sum);
+for (i = 0; i < count; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
+sum += i;
+}
+}
+printf("%d\n", sum);
 
-    return (0);
+return (0);
 }
