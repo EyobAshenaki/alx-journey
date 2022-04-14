9c1f0aca8331e895fdb1c2571cc49c3f1a0cd112
EyobAshenaki <ethioeyoba@gmail.com>
2022-04-14 10:51:48 +0300

    add main.c main.h 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * return - null
+ */
+
+void print_alphabet(void)
+{
+    for (char i = 'a'; i <= 'z'; i++)
+    {
+        _putchar(i);
+    }
+    _putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * return - 0 on Success
+ */
+
+int main(void)
+{
+    print_alphabet();
+
+    return (0);
+}
